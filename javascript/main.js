3304bc1e5ec9880cc8a614cabfe41d1fcd2588f8
Navdeep Singh Gambhir <harshgambhir969@gmail.com>
2022-05-13 16:21:48 +0530

    changes done

--- javascript/main.js
+++ javascript/main.js
@@ -6,25 +6,7 @@
 	    } else {
 	      $('.navbar').removeClass("sticky");
 	    }
-
-	    // scroll-up button show/hide script
-	    if (this.scrollY > 500) {
-	      $('.scroll-up-btn').addClass("show");
-	    } else {
-	      $('.scroll-up-btn').removeClass("show");
-	    }
 	  });
-
-	  // slide-up script
-	$('.scroll-up-btn').click(function() {
-	  $('html').animate({
-	    scrollTop: 0
-	  });
-	  // removing smooth scroll on slide-up button click
-	  $('html').css("scrollBehavior", "auto");
-	});
-
-
 	  var windowHeight = $(window).height(),
 	    topSection = $('master-section');
 	  topSection.css('height', windowHeight);
